e50405647edcd39a57ee194c6df28849c59d3d7e
mydongistiny <jaysonedson@gmail.com>
2016-08-23 19:26:59 -0700

    Angler: NRD90M
    Test2.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

---
+++ angler/device-vendor.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2016 The Pure Nexus Project
+# Copyright (C) 2016 Benzo Rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+LOCAL_PATH := $(my-dir)
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
+# Prebuilt APKs
+PRODUCT_PACKAGES += \
+    CABLService \
+    HwMMITest \
+    HwSarControlService \
+    ims \
+    qcrilmsgtunnel \
+    SetupSmartDeviceOverlay \
+    TimeService \
+    Tycho 
+
+# Prebuilt jars
+PRODUCT_PACKAGES += \
+    qcrilhook
+
+# Prebuilt privileged APKs
+PRODUCT_PACKAGES += \
+    CallStatistics \
+    CNEService \
+    ConnMO \
+    DCMO \
+    DiagMon \
+    DMService \
+    GCS \
+    HiddenMenu \
+    HotwordEnrollment \
+    SprintDM
+
+# Symlinks
+PRODUCT_PACKAGES += \
+    libimsmedia_jni.so \
+    libimscamera_jni.so \
+    libdmengine.so \
+    libdmjavaplugin.so \
+    libManufacture.so
+
+
